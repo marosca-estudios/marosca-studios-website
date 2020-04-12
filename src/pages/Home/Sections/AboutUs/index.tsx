5d8667bb89085507c6d941ffb22a1c59d073e914
Rita Ferreira <rita.ferreira@vakt.io>
2020-04-12 11:30:41 +0100

    aboutus beginning #2

--- src/pages/Home/Sections/AboutUs/index.tsx
+++ src/pages/Home/Sections/AboutUs/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import {
+  Section,
+  TopBar,
+  Container,
+  ContainerWithText,
+  Content, 
+  Title, TextAboutUs, 
+  ImageAboutUs
+} from './styled'
+import images from './images'
+import theme from 'global/theme'
 import { StyledSection } from 'styledComponents/Common'
+
 
 const AboutUs = () =>  (
   <StyledSection>
-
+    <TopBar />
+    <Container>
+      <ContainerWithText>
+        <Content>
+          <Title>Lovers of <br /> digital Narratives</Title>
+          <TextAboutUs>
+            <b>We are a web development studio based in Lisbon.</b>
+            Our passion lies in empowering big-thinkers and bringing their ideas to life by
+            providing full product-development, from wireframe to production.
+          </TextAboutUs>
+        </Content>
+      </ContainerWithText>
+      <ImageAboutUs />
+    </Container>
   </StyledSection>
 );
 
-export default AboutUs
+export default AboutUs;
--- src/pages/Home/Sections/AboutUs/styled.ts
+++ src/pages/Home/Sections/AboutUs/styled.ts
@@ -1,1 +1,47 @@
 import styled, { css, keyframes } from 'styled-components'
+import { mqMax, mqMin } from 'utils/mediaQuery'
+import theme from 'global/theme'
+import img from './images/img1.jpg'
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100vh;
+  position: relative;
+`
+
+export const TopBar = styled.div`
+  display: flex;
+  height: 10%;
+  width: 100%;
+
+  position: absolute;
+  top: 0rem;
+
+  background: ${theme.colors.SALMON};
+` 
+export const Container = styled.div`
+  background: ${theme.colors.SALMON};
+`
+export const ContainerWithText = styled.div`
+  display: flex;
+  content-align: center;
+  background:  ${theme.colors.BLACK};
+`
+
+export const Content = styled.div`
+`
+
+export const Title = styled.h1`
+  color:${theme.colors.SALMON};
+`
+export const TextAboutUs = styled.h4`
+  color:${theme.colors.WHITE};
+`
+
+export const ImageAboutUs = styled.image`
+  src:
+`
+
